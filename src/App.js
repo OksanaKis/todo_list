46b11fc48ec72b62a13d635de54c8c647700663b
OksanaKis <oksanakisetoja@gmail.com>
2021-11-22 16:00:35 +0200

    adding inputTextHandler function & bind it with state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React, {useState} from 'react';
 import './App.css';
 import Form from './components/Form';
 import ToDoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("");
+
   return (
     <div className="App">
       <header>
         <h1>My Todo List</h1>
       </header>
-      <Form/>
+      <Form setInputText={setInputText}/>
       <ToDoList/>
     </div>
   );
