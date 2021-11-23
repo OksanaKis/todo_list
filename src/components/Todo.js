c762843b29f1ce8f13d528c3f70818ab2c53edd9
OksanaKis <oksanakisetoja@gmail.com>
2021-11-23 12:41:41 +0200

    adding completeHandler function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,10 +7,21 @@ const Todo = ({text, todo, todos, setTodos}) => {
         console.log(todo);
     };
 
+    const completeHandler = () => {
+        setTodos(todos.map(el => {
+            if(el.id === todo.id) {
+                return {
+                    ...el, completed: !el.completed
+                }
+            }
+            return el;
+        }))
+    }
+
     return(
         <div className="todo">
-            <li className="todo-item">{text}</li>
-            <button className="complete-btn"><i className="fas fa-check"></i></button>
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+            <button onClick={completeHandler} className="complete-btn"><i className="fas fa-check"></i></button>
             <button onClick={deleteHandler} className="trash-btn"><i className="fas fa-trash"></i></button>
         </div>
     );
